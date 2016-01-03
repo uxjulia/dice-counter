afbcbc33580d4bbfc18d2dacd79477d55d647151
Julia <julia@8thsq.co>
2016-01-02 22:55:13 -0500

    add turn indicator and player names

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -33,15 +33,96 @@
     $(p).prependTo(target);
   }
 
-  function displayUsers(n){
-    var i;
-    for (i=0; i < n; i++) {
-      var span = $("<span/>", {
-        "class": "glyphicon glyphicon-user"
-      });
-      var target = $("div.user-icons");
-      $(span).appendTo(target)
+  function displayUsers(n) {
+    if (n==0) {
+      nextUp(0);
+    } else {
+      var i;
+      for (i = 0; i < n; i++) {
+        var span = $("<span/>", {
+          "class": "glyphicon glyphicon-user",
+          id: i + 1
+        });
+        var target = $("div.user-icons");
+        $(span).appendTo(target)
+      }
     }
+  }
+
+  function displayUserInputs(n) {
+    var target = $("div#playerNameInputs");
+    $(target).empty();
+    if (n==0){
+      console.log("No Players");
+    } else {
+      var i;
+      for (i = 0; i < n; i++) {
+        var ix = i + 1;
+        var input = $("<input/>", {
+          "class": "form-control",
+          id: ix
+          //placeholder: "Player " + ix + "'s Name"
+        });
+        var label = $("<label/>", {
+          "class":"control-label",
+          "for": input.id,
+          text: "Player " + ix + "'s Name"
+        });
+        var group = $("<div/>", {
+          "class":"form-group"
+        });
+        $(label).appendTo(group);
+        $(input).appendTo(group);
+        $(group).appendTo(target);
+      }
+    }
+  }
+
+  function highlightUser() {
+    var currentUser = $(".turn");
+    var next;
+    var removeTurn = function(x) {
+      $(x).removeClass('turn');
+    };
+    if ($(currentUser).length == "0") {
+      $("span.glyphicon-user:first-child").addClass('turn');
+    } else {
+      if ($(currentUser).attr("id") == $(".glyphicon-user").length) {
+        removeTurn(currentUser);
+        next = "1";
+      } else {
+        var u = parseFloat(($(currentUser).attr("id")));
+        removeTurn(currentUser);
+        next = (u + 1);
+      }
+    }
+    $("span#" + next + ".glyphicon-user").addClass('turn');
+    if ($("span.glyphicon-user").length != "0") {
+      nextUp(next);
+    }
+  }
+
+  function nextUp(x, name) {
+    var label = $("label#nextUp");
+    if (x == undefined) {
+      x = "1";
+    }
+    if ( x == "0" ) {
+      $("div#nextUpContainer").attr("style","display:none");
+      return;
+    }
+    if (name == null) {
+      name = "Player " + x;
+    }
+    $(label)
+      .parent().attr("style", "display:block")
+      .end()
+      .text("Next up: " + name);
+  }
+
+  function setUser(elem) {
+    $("span.turn").removeClass('turn');
+    $(elem).addClass('turn');
   }
 
   $(document).ready(function() {
@@ -100,16 +181,33 @@
       $("span#total-txt").html("Total Rolls: " + count);
     };
 
-    var numbers = $("button").not(".reset");
+    var numbers = $("button.digit");
 
     $(numbers).on('click', function () {
       logDiceRoll(this);
       logHistory(this);
       setTotalRolls();
+      highlightUser();
     });
 
+    $("button#settingsBtn").on('click',function() {
+      var settingsBtnSpan = $("span#settingsText");
+      if (settingsBtnSpan.html() == "Show Settings") {
+        settingsBtnSpan.html("Hide Settings");
+      } else {
+        settingsBtnSpan.html("Show Settings");
+
+      }
+    });
     $("button#reset").on('click', function () {
       resetData();
+      setUser($("span.glyphicon-user:first-child"));
+      nextUp(1);
+    });
+
+    $(".user-icons").on('click','span.glyphicon-user', function(){
+      setUser(this);
+      nextUp(this.id);
     });
 
     $("button#undo").on('click', function(){
@@ -127,7 +225,9 @@
       var target = $("div.user-icons");
       $(target).empty();
       var n = $(this).val();
+      displayUserInputs(n);
       displayUsers(n);
+      highlightUser();
     });
 
   });
